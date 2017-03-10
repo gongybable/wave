43252a08b60ed4c463e91de5f0a6b854bc4ff49d
Yibo Gong <gongybable@gmail.com>
2017-03-09 22:53:34 -0500

    add model for query

--- api/lib/mysql.js
+++ api/lib/mysql.js
@@ -4,16 +4,30 @@
  * Wrapper around mysql-promise. Handles connection to server.
  */
 var mysql  = require('promise-mysql'),
+	conf,
     pool;
 
 module.exports = {
     // Get configuration parameters and connect to database.
     config: function (cfg) {
+    	conf = cfg;
         pool =  mysql.createPool(cfg);
     },
 
     // Return the promised connection pool
     promiseDb: function() {
         return pool;
+    },
+
+    // Create a new connection for transaction purpose
+    newConnection: function() {
+    	return mysql.createConnection({
+    		host: conf.host,
+    		user: conf.user,
+    		password: conf.password,
+    		database: conf.database
+    	}).then(function (conn) {
+    		return conn;
+    	});
     }
 };
--- api/models/index.js
+++ api/models/index.js
@@ -1,7 +1,37 @@
 'use strict';
 
-module.exports = function IndexModel() {
-    return {
-        name: 'index'
-    };
+var mysql = require('../lib/mysql'),
+    self;
+
+self = module.exports = {
+    /**
+     * @function promiseGetReporId
+     * Given a reportId, will return the reportId if exists in database.
+     * Otherwise return empty array
+     */
+    promiseGetReporId: function (id) {
+        mysql.promiseDb().query('SELECT reportId From report WHERE reportId = ?', [id]);
+    },
+
+    /**
+     * @function promiseInsertPayrollData
+     * Transaction:
+     * 1. Insert csv data into table payroll
+     * 2. Insert reportId into table report
+     */
+    promiseInsertPayrollData: function (csvRows, reportId) {
+        var conn = mysql.newConnection();
+
+        return conn.beginTransaction().then(function () {
+            return conn.query('INSERT INTO report (reportId) Values (?)', [reportId]);
+        }).then(function () {
+            return conn.query('INSERT INTO payroll VALUES ?', [csvRows]);
+        }).then(function () {
+            return conn.commit();
+        }).catch(function (err) {
+            return conn.rollback().then(function () { throw err; });
+        }).finally(function () {
+            return conn.end();
+        });
+    }
 };
